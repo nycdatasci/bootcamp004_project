9d6e1ae17047bfd27831cc141008024f0db05883
Amy Ma <amyma9145@gmail.com>
2016-02-25 20:13:17 -0500

    presentation

--- Project3-WebScraping/amyma/python/extract.py
+++ Project3-WebScraping/amyma/python/extract.py
@@ -5,30 +5,10 @@ try:
     import Image
 except ImportError:
     from PIL import Image
-from colormath.color_objects import LabColor, sRGBColor
-from colormath.color_conversions import convert_color
 from colormath.color_diff import delta_e_cie1976
 import pandas as pd
 import csv
 
-black=LabColor (lab_l=0.0000,lab_a=0.0000,lab_b=0.0000)
-gray=LabColor (lab_l=53.5850,lab_a=-0.0003,lab_b=-0.0051)
-sliver=LabColor (lab_l=77.7044,lab_a=-0.0004,lab_b=-0.0069)
-white=LabColor (lab_l=100.0000,lab_a=-0.0005,lab_b=-0.0086)
-maroon=LabColor (lab_l=25.5344,lab_a=48.0439,lab_b=38.0559)
-red=LabColor (lab_l=53.2390,lab_a=80.0905,lab_b=67.2014)
-olive=LabColor (lab_l=51.8687,lab_a=-12.9319,lab_b=56.6742)
-yellow=LabColor (lab_l=97.1388,lab_a=-21.5578,lab_b=94.4773)
-green=LabColor (lab_l=46.2276,lab_a=-51.6986,lab_b=49.8970)
-lime=LabColor (lab_l=87.7350,lab_a=-86.1829,lab_b=83.1795)
-teal=LabColor (lab_l=48.2545,lab_a=-28.8437,lab_b=-8.4814)
-aqua=LabColor (lab_l=91.1140,lab_a=-48.0832,lab_b=-14.1386)
-navy=LabColor (lab_l=12.9734,lab_a=47.5046,lab_b=-64.7053)
-blue=LabColor (lab_l=32.2994,lab_a=79.1914,lab_b=-107.8655)
-orange=LabColor (lab_l=74.9347,lab_a=23.9292,lab_b=78.9486)
-purple=LabColor (lab_l=29.7843,lab_a=58.9285,lab_b=-36.4932)
-fuchsia=LabColor (lab_l=60.3236,lab_a=98.2353,lab_b=-60.8350)
-brown=LabColor (lab_l=37.4691,lab_a=26.4404,lab_b=40.9820)
 
 Point = namedtuple('Point', ('coords', 'n', 'ct'))
 Cluster = namedtuple('Cluster', ('points', 'center', 'n'))
@@ -85,22 +65,6 @@ def colorz(filename, n=5):
 #map(rtoh, rgbs)# retrun rgbs directly
 #rgbs # return the html color code
 # I need  a big loop for all the images I got, with the key as the image name
-
-
-
-def euclidean(p1, p2):
-    return sqrt(sum([
-                     (p1.coords[i] - p2.coords[i]) ** 2 for i in range(p1.n)
-                     ]))
-
-def calculate_center(points, n):
-    vals = [0.0 for i in range(n)]
-    plen = 0
-    for p in points:
-        plen += p.ct
-        for i in range(n):
-            vals[i] += (p.coords[i] * p.ct)
-    return Point([(v / plen) for v in vals], n, 1)
 
 def kmeans(points, k, min_diff):
     clusters = [Cluster([p], p, p.n) for p in random.sample(points, k)]
